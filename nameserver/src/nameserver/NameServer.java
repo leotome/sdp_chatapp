203691d9e3ee7070ab99fc4a9a1f31f24602c5af
Leonardo Oliveira <30002511@students.ual.pt>
2022-01-02 10:01:24 +0000

    Implementing PIN policy for backend

--- nameserver/src/nameserver/NameServer.java
+++ nameserver/src/nameserver/NameServer.java
@@ -7,12 +7,14 @@ public class NameServer {
 	private Socket socket;
 	private Map<String, Integer> Users;
 	private Integer NameServer_Port;
+	private List<Integer> AllowedPINs;
 	
 	public Boolean ShowDebug = false;
 
 	public NameServer(Integer NameServer_Port) {
 		this.NameServer_Port = NameServer_Port;
 		this.Users = new HashMap<String, Integer>();
+		this.setPINPolicy();
 	}
 	
 	public Socket getSocket() {
@@ -31,12 +33,23 @@ public class NameServer {
 		return false;
 	}
 	
+	public void setPINPolicy() {
+		List<Integer> AllowedPINs = new ArrayList<Integer>();
+		for(Integer i = 8000; i <= 8010; i++) {
+			AllowedPINs.add(i);
+		}
+		this.AllowedPINs = AllowedPINs;
+	}
+	
 	public String registerUser(String Nickname, Integer PIN) {
 		if(this.Users.containsKey(Nickname) == true) {
 			return "ERROR: This user already exists";
 		}
 		if(this.Users.containsValue(PIN) == true) {
 			return "ERROR: This PIN is being used";
+		}
+		if(AllowedPINs.contains(PIN) == false) {
+			return "ERROR: PIN does not match policy.";
 		}
 		this.Users.put(Nickname, PIN);
 		printDebug("User '" + Nickname + "' was registered sucessfully with PIN '" + PIN + "'");
@@ -66,8 +79,6 @@ public class NameServer {
 	
 	public void sendRequest(String address, String payload) {
 		String[] ToAddress = address.split(":");
-		printDebug("Destination: '" + address + "'");
-		printDebug("Payload to send: '" + payload + "'");
 		this.getSocket().sendDatagramPacket(Integer.valueOf(ToAddress[1]), payload, ToAddress[0]);
 	}
 	
@@ -76,7 +87,6 @@ public class NameServer {
 		Map<String, String> req = this.formatRequest(request);
 		Map<String, String> res = new HashMap<String, String>();
 		res.put("APP", "NS");
-		System.out.println(req);
 		if(req.get("Id") != null) {res.put("Id", req.get("Id"));}
 		switch(req.get("OP")) {
 			case "REGISTER":			
@@ -84,9 +94,12 @@ public class NameServer {
 				Integer REGISTER_PIN = Integer.valueOf(req.get("PIN"));
 				String REGISTER_Result = this.registerUser(REGISTER_Nickname, REGISTER_PIN);
 				
+				res.put("TYPE", "REGISTER");
 				res.put("CODE", (REGISTER_Result.split(": ")[0] == "SUCCESS") ? "201" : "403");
 				res.put("STATUS", REGISTER_Result.split(": ")[0]);
 				res.put("MESSAGE", REGISTER_Result.split(": ")[1]);
+				res.put("NK", req.get("NK"));
+				res.put("PIN", req.get("PIN"));
 				
 				this.sendRequest(sender, res.toString());
 				break;
@@ -95,9 +108,12 @@ public class NameServer {
 				Integer LOGIN_PIN = Integer.valueOf(req.get("PIN"));
 				Boolean LOGIN_Success = this.loginUser(LOGIN_Nickname, LOGIN_PIN);
 				
+				res.put("TYPE", "LOGIN");
 				res.put("CODE", (LOGIN_Success == true) ? "200" : "403");
 				res.put("STATUS", (LOGIN_Success == true) ? "SUCCESS" : "ERROR");
 				res.put("MESSAGE", "The credentials are correct!");
+				res.put("NK", req.get("NK"));
+				res.put("PIN", req.get("PIN"));				
 				
 				this.sendRequest(sender, res.toString());
 				break;
@@ -105,6 +121,7 @@ public class NameServer {
 				String RECOVER_Nickname = req.get("NK");
 				Integer RECOVER_PIN = this.recoverPIN(RECOVER_Nickname);
 				
+				res.put("TYPE", "RECOVER");
 				res.put("CODE", (RECOVER_PIN != -1) ? "200" : "403");
 				res.put("STATUS", (RECOVER_PIN != -1) ? "SUCCESS" : "ERROR");
 				res.put("MESSAGE", (RECOVER_PIN != -1) ? "Your PIN is: " + RECOVER_PIN : "This user is not registered.");
